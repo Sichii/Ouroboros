ee59cb4d31d6e72cdcff33ec370598fc66967279
sichii <despereaux@live.com>
2024-02-02 06:54:15 -0500

    DllImport to LibraryImport - also minor fix for post build event on ouroboros

--- Injector32/UnsafeNativeMethods.cs
+++ Injector32/UnsafeNativeMethods.cs
@@ -2,13 +2,13 @@
 
 namespace Injector32;
 
-public static class UnsafeNativeMethods
+public static partial class UnsafeNativeMethods
 {
-    [DllImport("kernel32.dll")]
-    public static extern WaitEventResult WaitForSingleObject(nint hObject, int timeout);
+    [LibraryImport("kernel32.dll")]
+    public static partial WaitEventResult WaitForSingleObject(nint hObject, int timeout);
     
-    [DllImport("kernel32")]
-    public static extern nint CreateRemoteThread(
+    [LibraryImport("kernel32")]
+    public static partial nint CreateRemoteThread(
         nint hProcess,
         nint lpThreadAttributes,
         nint dwStackSize,
@@ -17,49 +17,44 @@ public static class UnsafeNativeMethods
         uint dwCreationFlags,
         out nint lpThreadId);
 
-    [DllImport("kernel32.dll")]
-    public static extern int ResumeThread(nint hThread);
+    [LibraryImport("kernel32.dll")]
+    public static partial int ResumeThread(nint hThread);
 
-    [DllImport(
-        "kernel32.dll",
-        ExactSpelling = true,
-        BestFitMapping = false,
-        ThrowOnUnmappableChar = true)]
-    public static extern nuint GetProcAddress(nint hModule, string procName);
+    [LibraryImport("kernel32.dll")]
+    public static partial nuint GetProcAddress(nint hModule, [MarshalAs(UnmanagedType.LPStr)] string procName);
 
-    [DllImport("kernel32.dll")]
-    public static extern nint OpenProcess(ProcessAccessFlags access, bool inheritHandle, int processId);
+    [LibraryImport("kernel32.dll")]
+    public static partial nint OpenProcess(ProcessAccessFlags access, [MarshalAs(UnmanagedType.Bool)] bool inheritHandle, int processId);
 
-    [DllImport(
-        "kernel32.dll",
-        CharSet = CharSet.Auto,
-        BestFitMapping = false,
-        ThrowOnUnmappableChar = true)]
-    public static extern nint GetModuleHandle(string lpModuleName);
+    [LibraryImport("kernel32.dll", EntryPoint = "GetModuleHandleA")]
+    public static partial nint GetModuleHandle([MarshalAs(UnmanagedType.LPStr)] string lpModuleName);
 
-    [DllImport("kernel32.dll")]
-    public static extern bool CloseHandle(nint handle);
+    [LibraryImport("kernel32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool CloseHandle(nint handle);
     
-    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-    public static extern nint VirtualAllocEx(
+    [LibraryImport("kernel32.dll", SetLastError = true)]
+    public static partial nint VirtualAllocEx(
         nint hProcess,
         nint lpAddress,
         nint dwSize,
         uint flAllocationType,
         uint flProtect);
 
-    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-    public static extern bool VirtualFreeEx(
+    [LibraryImport("kernel32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool VirtualFreeEx(
         nint hProcess,
         nint lpAddress,
         nuint dwSize,
         uint dwFreeType);
 
-    [DllImport("kernel32.dll", BestFitMapping = false, ThrowOnUnmappableChar = true)]
-    public static extern bool WriteProcessMemory(
+    [LibraryImport("kernel32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool WriteProcessMemory(
         nint hProcess,
         nint lpBaseAddress,
-        string lpBuffer,
+        [MarshalAs(UnmanagedType.LPStr)] string lpBuffer,
         nuint nSize,
         out nint lpNumberOfBytesWritten);
 }
--- Ouroboros/PInvoke/UnsafeNativeMethods.cs
+++ Ouroboros/PInvoke/UnsafeNativeMethods.cs
@@ -4,52 +4,53 @@ using Ouroboros.Defintions;
 
 namespace Ouroboros.PInvoke;
 
-public static class UnsafeNativeMethods
+public static partial class UnsafeNativeMethods
 {
-    [DllImport("kernel32.dll")]
-    public static extern WaitEventResult WaitForSingleObject(nint hObject, int timeout);
+    [LibraryImport("kernel32.dll")]
+    public static partial WaitEventResult WaitForSingleObject(nint hObject, int timeout);
 
     #region Thumbnail Manipulation
 
-    [DllImport("dwmapi.dll")]
-    public static extern int DwmRegisterThumbnail(nint dest, nint src, out nint thumb);
+    [LibraryImport("dwmapi.dll")]
+    public static partial int DwmRegisterThumbnail(nint dest, nint src, out nint thumb);
 
     [DllImport("dwmapi.dll")]
     public static extern int DwmUpdateThumbnailProperties(nint hThumb, ref ThumbnailProperties props);
 
-    [DllImport("dwmapi.dll")]
-    public static extern int DwmUnregisterThumbnail(nint thumb);
+    [LibraryImport("dwmapi.dll")]
+    public static partial int DwmUnregisterThumbnail(nint thumb);
 
     #endregion
 
     #region Thread Manipulation
 
-    [DllImport("kernel32")]
-    public static extern nint CreateRemoteThread(
+    [LibraryImport("kernel32")]
+    public static partial nint CreateRemoteThread(
         nint hProcess, nint lpThreadAttributes, nint dwStackSize, nuint lpStartAddress, nint lpParameter, uint dwCreationFlags,
         out nint lpThreadId);
 
-    [DllImport("kernel32.dll")]
-    public static extern int ResumeThread(nint hThread);
+    [LibraryImport("kernel32.dll")]
+    public static partial int ResumeThread(nint hThread);
 
     #endregion
 
     #region Process Manipulation
 
-    [DllImport("kernel32.dll", ExactSpelling = true, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-    public static extern nuint GetProcAddress(nint hModule, string procName);
+    [LibraryImport("kernel32.dll")]
+    public static partial nuint GetProcAddress(nint hModule, [MarshalAs(UnmanagedType.LPStr)] string procName);
 
-    [DllImport("kernel32.dll", BestFitMapping = false, ThrowOnUnmappableChar = true)]
-    public static extern bool CreateProcess(
-        string applicationName, string? commandLine, nint processAttributes, nint threadAttributes, bool inheritHandles,
+    [LibraryImport("kernel32.dll", EntryPoint = "CreateProcessW", StringMarshalling = StringMarshalling.Utf16)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool CreateProcess(
+        string applicationName, string? commandLine, nint processAttributes, nint threadAttributes, [MarshalAs(UnmanagedType.Bool)] bool inheritHandles,
         ProcessCreationFlags creationFlags, nint environment, string? currentDirectory, ref StartupInformation startupInfo,
         out ProcessInformation processInfo);
 
-    [DllImport("kernel32.dll")]
-    public static extern nint OpenProcess(ProcessAccessFlags access, bool inheritHandle, int processId);
+    [LibraryImport("kernel32.dll")]
+    public static partial nint OpenProcess(ProcessAccessFlags access, [MarshalAs(UnmanagedType.Bool)] bool inheritHandle, int processId);
 
-    [DllImport("kernel32.dll", CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-    public static extern nint GetModuleHandle(string lpModuleName);
+    [LibraryImport("kernel32.dll", EntryPoint = "GetModuleHandleA")]
+    public static partial nint GetModuleHandle([MarshalAs(UnmanagedType.LPStr)] string lpModuleName);
     
     [DllImport("psapi.dll", SetLastError = true)]
     public static extern bool EnumProcessModulesEx(nint hProcess, [Out] nint[] lphModule, int cb, out int lpcbNeeded, DwFilterFlag dwFilterFlag);
@@ -57,84 +58,98 @@ public static class UnsafeNativeMethods
     [DllImport("psapi.dll", SetLastError = true)]
     public static extern uint GetModuleFileNameEx(nint hProcess, nint hModule, [Out] StringBuilder lpBaseName, int nSize);
 
-    [DllImport("kernel32.dll")]
-    public static extern bool CloseHandle(nint handle);
+    [LibraryImport("kernel32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool CloseHandle(nint handle);
 
     #endregion
 
     #region Memory Manipulation
 
-    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-    public static extern nint VirtualAllocEx(nint hProcess, nint lpAddress, nint dwSize, uint flAllocationType, uint flProtect);
+    [LibraryImport("kernel32.dll", SetLastError = true)]
+    public static partial nint VirtualAllocEx(nint hProcess, nint lpAddress, nint dwSize, uint flAllocationType, uint flProtect);
 
-    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-    public static extern bool VirtualFreeEx(nint hProcess, nint lpAddress, nuint dwSize, uint dwFreeType);
+    [LibraryImport("kernel32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool VirtualFreeEx(nint hProcess, nint lpAddress, nuint dwSize, uint dwFreeType);
 
-    [DllImport("kernel32.dll", BestFitMapping = false, ThrowOnUnmappableChar = true)]
-    public static extern bool WriteProcessMemory(
-        nint hProcess, nint lpBaseAddress, string lpBuffer, nuint nSize, out nint lpNumberOfBytesWritten);
+    [LibraryImport("kernel32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool WriteProcessMemory(
+        nint hProcess,
+        nint lpBaseAddress,
+        [MarshalAs(UnmanagedType.LPStr)] string lpBuffer,
+        nuint nSize,
+        out nint lpNumberOfBytesWritten);
 
-    [DllImport("kernel32.dll")]
-    public static extern bool WriteProcessMemory(nint hProcess, nint baseAddress, nint buffer, nint count, out int bytesWritten);
+    [LibraryImport("kernel32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool WriteProcessMemory(nint hProcess, nint baseAddress, nint buffer, nint count, out int bytesWritten);
 
-    [DllImport("kernel32.dll")]
-    public static extern bool ReadProcessMemory(nint hProcess, nint baseAddress, nint buffer, nint count, out int bytesRead);
+    [LibraryImport("kernel32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool ReadProcessMemory(nint hProcess, nint baseAddress, nint buffer, nint count, out int bytesRead);
 
     #endregion
 
     #region Window Manipulation
 
-    [DllImport("user32.dll")]
-    public static extern bool ShowWindowAsync(nint hWnd, ShowWindowFlags nCmdShow);
-
-    [DllImport("user32.dll")]
+    [LibraryImport("user32.dll")]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool ShowWindow(nint hWnd, ShowWindowFlags nCmdShow);
+    public static partial bool ShowWindowAsync(nint hWnd, ShowWindowFlags nCmdShow);
 
-    [DllImport("user32.dll", SetLastError = true)]
-    public static extern bool MoveWindow(nint hwnd, int x, int y, int nWidth, int nHeight, bool bRepaint);
-
-    [DllImport("user32.dll")]
+    [LibraryImport("user32.dll")]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool IsWindowVisible(nint hWnd);
-    
-    [DllImport("user32.dll")]
-    public static extern uint GetDpiForWindow(nint hwnd);
-    
-    [DllImport("user32.dll")]
-    public static extern bool AdjustWindowRectEx(ref Rect lpRect, WindowStyleFlags dwStyle, bool bMenu, uint dwExStyle);
-    
-    [DllImport("Shcore.dll")]
-    public static extern nint GetDpiForMonitor(nint hmonitor, int dpiType, out uint dpiX, out uint dpiY);
+    public static partial bool ShowWindow(nint hWnd, ShowWindowFlags nCmdShow);
 
-    [DllImport("user32.dll")]
-    public static extern nint MonitorFromWindow(nint hwnd, uint dwFlags);
+    [LibraryImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool MoveWindow(nint hwnd, int x, int y, int nWidth, int nHeight, [MarshalAs(UnmanagedType.Bool)] bool bRepaint);
+
+    [LibraryImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool IsWindowVisible(nint hWnd);
+    
+    [LibraryImport("user32.dll")]
+    public static partial uint GetDpiForWindow(nint hwnd);
+    
+    [LibraryImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool AdjustWindowRectEx(ref Rect lpRect, WindowStyleFlags dwStyle, [MarshalAs(UnmanagedType.Bool)] bool bMenu, uint dwExStyle);
+    
+    [LibraryImport("Shcore.dll")]
+    public static partial nint GetDpiForMonitor(nint hmonitor, int dpiType, out uint dpiX, out uint dpiY);
+
+    [LibraryImport("user32.dll")]
+    public static partial nint MonitorFromWindow(nint hwnd, uint dwFlags);
     
     #endregion
 
     #region GetWindow
 
-    [DllImport("user32.dll")]
-    public static extern bool GetWindowRect(nint hwnd, ref Rect rectangle);
+    [LibraryImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool GetWindowRect(nint hwnd, ref Rect rectangle);
 
-    [DllImport("user32.dll")]
-    public static extern bool GetClientRect(nint hWnd, ref Rect rectangle);
+    [LibraryImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool GetClientRect(nint hWnd, ref Rect rectangle);
 
-    [DllImport("user32.dll")]
-    public static extern WindowStyleFlags GetWindowLong(nint hWnd, WindowFlags nIndex);
+    [LibraryImport("user32.dll", EntryPoint = "GetWindowLongW")]
+    public static partial WindowStyleFlags GetWindowLong(nint hWnd, WindowFlags nIndex);
 
     #endregion
 
     #region SetWindow
 
-    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
-    public static extern int SetWindowText(nint hWnd, string text);
+    [LibraryImport("user32.dll", StringMarshalling = StringMarshalling.Utf16)]
+    public static partial int SetWindowText(nint hWnd, string text);
 
-    [DllImport("user32.dll")]
-    public static extern int SetWindowLong(nint hWnd, WindowFlags nIndex, WindowStyleFlags dwNewLong);
+    [LibraryImport("user32.dll")]
+    public static partial int SetWindowLong(nint hWnd, WindowFlags nIndex, WindowStyleFlags dwNewLong);
 
-    [DllImport("User32.dll")]
-    public static extern int SetForegroundWindow(int hWnd);
+    [LibraryImport("User32.dll")]
+    public static partial int SetForegroundWindow(int hWnd);
 
     #endregion
 }
